6fb31488d41478666d2905e35d9c2fd6d049b49d
Bryan Rinders <11392566-bryanrinders@users.noreply.gitlab.com>
2024-04-28 18:47:31 +0200

    simplify definition `org-publish-timestamp-directory'

--- build-site.el
+++ build-site.el
@@ -26,11 +26,8 @@
 ;; move the timestamps directory when running this script on my
 ;; personal machine
 (setq org-publish-timestamp-directory
-      (let ((my-project-dir (getenv "MY_PROJECTS_DIR")))
-        (if my-project-dir
-            (concat my-project-dir "/bryanrinders.xyz/.org-timestamps/")
-          "~/.org-timestamps/")))
-
+      (file-name-concat (or (vc-root-dir) "~")
+                        ".org-timestamps/"))
 
 ;; load some programming languages for syntax highlighting
 (org-babel-do-load-languages
